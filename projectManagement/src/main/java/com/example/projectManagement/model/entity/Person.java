a56d014094f0f29c953c5c394fb045a33b367635
Asus <haniyehghahrmani@gmail.com>
2024-09-11 19:52:38 +0330

    commit

---
+++ projectManagement/src/main/java/com/example/projectManagement/model/entity/Person.java
@@ -0,0 +1,61 @@
+package com.example.projectManagement.model.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+
+@Entity(name = "PersonEntity")
+@Table(name = "PersonTbl")
+public class Person {
+    @Id
+    @SequenceGenerator(name = "personSeq", sequenceName = "person_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "personSeq")
+    @Column(name = "person_id")
+    private Long id;
+
+    @Column(name = "person_name",  columnDefinition = "NVARCHAR2(50)")
+    @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,50}$", message = "Invalid Name")
+    private String name;
+
+    @Column(name = "person_lastname", columnDefinition = "NVARCHAR2(50)")
+    @Pattern(regexp = "^[a-zA-Zآ-ی\\s]{3,50}$", message = "Invalid Family")
+    @Size(min = 3, max = 50, message = "Last Name must be between 3 and 50 characters")
+    @NotBlank(message = "Should Not Be Null")
+    private String lastname;
+
+    @Column(name = "person_national_id", length = 10, unique = true)
+    @Pattern(regexp = "^[0-9]{1,10}$", message = "Invalid National ID")
+    @Size(min = 1, max = 10, message = " National ID must be between 1 and 10 characters")
+    @NotBlank(message = "Should Not Be Null")
+    private String nationalId;
+
+    @Column(name = "person_birthdate")
+    @Past(message = "Invalid Birth Date")
+//    @NotNull(message = "Should Not Be Null")
+    private LocalDate birthdate;
+
+
+    @Column(name = "person_age")
+    @NotNull(message = "Should Not Be Null")
+    private int age ;
+
+//    @Column(name = "person_gender")
+//    @Enumerated(EnumType.ORDINAL)
+////    @NotNull(message = "Should Not Be Null")
+//    private Gender gender;
+//
+//    @OneToOne(cascade = {CascadeType.MERGE ,CascadeType.PERSIST}, fetch = FetchType.LAZY)
+//    private User user;
+}
