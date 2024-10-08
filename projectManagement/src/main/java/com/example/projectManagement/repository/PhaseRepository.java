9e6d6ef599bb6ec3a5a6dd1b53fa8ad576f58010
Asus <haniyehghahrmani@gmail.com>
2024-10-08 21:25:54 +0330

    PhaseRepository updated

--- projectManagement/src/main/java/com/example/projectManagement/repository/PhaseRepository.java
+++ projectManagement/src/main/java/com/example/projectManagement/repository/PhaseRepository.java
@@ -1,6 +1,7 @@
 package com.example.projectManagement.repository;
 
 import com.example.projectManagement.model.entity.Phase;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PhaseRepository {
+public interface PhaseRepository extends JpaRepository<Phase,Long> {
 
     @Modifying
     @Query("update PhaseEntity oo set oo.deleted=true where oo.id=:id")
