a0630589d579233bb55c3ca05a3636d4d51589d3
royazargar <roya.zargar1992@gmail.com>
2024-10-15 19:31:05 +0330

    phase updated

--- projectManagement/src/main/java/com/example/projectManagement/repository/PhaseRepository.java
+++ projectManagement/src/main/java/com/example/projectManagement/repository/PhaseRepository.java
@@ -5,14 +5,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface PhaseRepository extends JpaRepository<Phase,Long> {
+public interface PhaseRepository extends JpaRepository<Phase, Long> {
 
     @Modifying
+    @Transactional
     @Query("update PhaseEntity oo set oo.deleted=true where oo.id=:id")
     void logicalRemove(Long id);
 
--- projectManagement/src/main/java/com/example/projectManagement/service/PhaseService.java
+++ projectManagement/src/main/java/com/example/projectManagement/service/PhaseService.java
@@ -22,6 +22,7 @@ public interface PhaseService {
 
     Long getPhaseCount();
 
+    @Transactional
     Phase logicalRemoveWithReturn(Long id) throws NoContentException;
 
     List<Phase> findPhaseByDeletedFalse();
